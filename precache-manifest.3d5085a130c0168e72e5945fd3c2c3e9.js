b6ab7736d0f9f2c8afafeb1a7b1a53cf1f6c7014
davidkc <dakchoy@gmail.com>
2021-12-14 13:34:18 +0400

    Updates

---
+++ precache-manifest.3d5085a130c0168e72e5945fd3c2c3e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185321345d6c643745b4cdb7170556b7",
+    "url": "/travel/index.html"
+  },
+  {
+    "revision": "a3a02f3b4dc886591deb",
+    "url": "/travel/static/css/main.b420149b.chunk.css"
+  },
+  {
+    "revision": "8cd18bff0ab931b3f589",
+    "url": "/travel/static/js/2.ad8a1fd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travel/static/js/2.ad8a1fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3a02f3b4dc886591deb",
+    "url": "/travel/static/js/main.edddb46e.chunk.js"
+  },
+  {
+    "revision": "830e51534c22e605d281",
+    "url": "/travel/static/js/runtime-main.64dcc6f4.js"
+  }
+]);
